60101a204fcd090ed7fabc773648b6e189bc6da2
AlexKly <Klyukvanstalker@gmail.com>
2022-12-21 20:57:53 +0300

    Added YOLO format for represantation bounding box coordinates

--- utils.py
+++ utils.py
@@ -13,10 +13,11 @@ class Utils:
 
 
 class AutoBoxing:
-    def __init__(self):
-        pass
+    def __init__(self, box_type='original'):
+        self.box_type = box_type
 
-    def detect_edges(self, filename):
+    @staticmethod
+    def detect_edges(filename):
         """
 
         :param filename:
@@ -37,9 +38,32 @@ class AutoBoxing:
 
         return edges
 
+    def convert_coordinates(self, box_coordinates, size):
+        """
+
+        :param box_coordinates:
+        :param size:
+        :return:
+        """
+        if self.box_type == 'original':
+            return (box_coordinates[0], box_coordinates[2]), (box_coordinates[1], box_coordinates[3])
+        elif self.box_type == 'yolo':
+            dw = 1. / size[0]
+            dh = 1. / size[1]
+            x = (box_coordinates[0] + box_coordinates[1]) / 2.0
+            y = (box_coordinates[2] + box_coordinates[3]) / 2.0
+            w = box_coordinates[1] - box_coordinates[0]
+            h = box_coordinates[3] - box_coordinates[2]
+            x = x * dw
+            w = w * dw
+            y = y * dh
+            h = h * dh
+
+            return x, y, w, h
+
     def create_box(self, edges):
         """
-        
+
         :param edges:
         :return:
         """
@@ -50,16 +74,27 @@ class AutoBoxing:
                 if edges[y][x] != 0:
                     nonzero_x_coords.append(x)
                     nonzero_y_coords.append(y)
+        box_coordinates = (
+            np.min(nonzero_x_coords),
+            np.max(nonzero_x_coords),
+            np.min(nonzero_y_coords),
+            np.max(nonzero_y_coords)
+        )
+        box_coordinates = self.convert_coordinates(
+            box_coordinates=box_coordinates,
+            size=(edges.shape[0], edges.shape[1])
+        )
 
-        return (np.min(nonzero_x_coords), np.min(nonzero_y_coords)), (np.max(nonzero_x_coords), np.max(nonzero_y_coords))
+        return box_coordinates
+
 
 
 if __name__ == '__main__':
-    ab = AutoBoxing()
+    ab = AutoBoxing(box_type='yolo')
     e = ab.detect_edges(filename='/home/aklyuev/PycharmProjects/ToyBoyDynamics/TOYBOY DYNAMICS/datasets/triangle_part/images/0.png')
     img = cv2.imread('/home/aklyuev/PycharmProjects/ToyBoyDynamics/TOYBOY DYNAMICS/datasets/triangle_part/images/0.png')
     box_c = ab.create_box(edges=e)
-    image = cv2.rectangle(img, box_c[0], box_c[1], (0, 0, 255), 2)
-    window_name = 'Image'
-    cv2.imshow(window_name, image)
-    cv2.waitKey(0)
+    #image = cv2.rectangle(img, box_c[0], box_c[1], (0, 0, 255), 2)
+    #window_name = 'Image'
+    #cv2.imshow(window_name, image)
+    #cv2.waitKey(0)
